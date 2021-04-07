65bd7edfda21c0bbd1621f254bdb787225c1411e
Andrii Symon <simchik333@gmail.com>
2021-04-07 17:24:25 +0300

    base code commit

---
+++ src/main/java/manager/PageFactoryManager.java
@@ -0,0 +1,13 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageFactoryManager {
+
+    WebDriver driver;
+
+    public PageFactoryManager(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
