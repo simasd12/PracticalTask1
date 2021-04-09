6ae867e15bdac89d50a855705fb5c944eb1a7675
Andrii Symon <simchik333@gmail.com>
2021-04-09 12:42:10 +0300

    fix cucumber reports

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
         features = "src/test/resources",
         glue = "stepdefinitions"
 )
